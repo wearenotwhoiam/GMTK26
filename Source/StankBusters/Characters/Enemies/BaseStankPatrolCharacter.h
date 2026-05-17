edc8495c9df8398e9b891defbd750cedc98c9895
wearenotwhoiam <weareryanmeyers@gmail.com>
2026-05-17 16:34:04 -0700

    hehehe

---
+++ Source/StankBusters/Characters/Enemies/BaseStankPatrolCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../BaseStankCharacter.h"
+#include "BaseStankPatrolCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STANKBUSTERS_API ABaseStankPatrolCharacter : public ABaseStankCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/StankBusters/Characters/MurcusPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseStankCharacter.h"
+#include "MurcusPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STANKBUSTERS_API AMurcusPlayerCharacter : public ABaseStankCharacter
+{
+	GENERATED_BODY()
+	
+};
