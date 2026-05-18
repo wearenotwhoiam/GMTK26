b89222e466d3b1e48f2577ecf878b746ff6d9e0a
wearenotwhoiam <weareryanmeyers@gmail.com>
2026-05-18 15:14:49 -0700

    Adding Attributes

---
+++ Source/StankBusters/Public/AbilitySystem/StankAttributeSet.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+//#pragma once
+//
+//#include "CoreMinimal.h"
+//#include "AttributeSet.h"
+//#include "AbilitySystem/StankAbilitySystemComponent.h"
+//
+//#include "StankAttributeSet.generated.h"
+//
+//#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+// 	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+// 	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+// 	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+// 	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+//UCLASS()
+//class STANKBUSTERS_API UStankAttributeSet : public UAttributeSet
+//{
+//	GENERATED_BODY()
+//public:
+//	UStankAttributeSet();
+//
+//	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+//	FGameplayAttributeData CurrentMucus;
+//	ATTRIBUTE_ACCESSORS(UStankAttributeSet, CurrentMucus);
+//	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+//	FGameplayAttributeData MaxMucus;
+//	ATTRIBUTE_ACCESSORS(UStankAttributeSet, MaxMucus);
+//
+//};
